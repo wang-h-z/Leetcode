61c3549eb11a64a171a66c5877f24d5dc7a650c1
WangHaozhen <whaozhen01@gmail.com>
2024-05-15 22:38:50 +0800

    fuck my life

--- Array/SlidingWindowMaximum.py
+++ Array/SlidingWindowMaximum.py
@@ -2,14 +2,36 @@ import heapq
 from typing import List
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
         heap = []
-        heapq.heapify(heap)
-        queue = nums
-        i = 0
-        for i in range(k):
-            heapq.heappush(heap, nums[i])
         
-        return heap
+        l, r = 0, k - 1
+        for l in range(r + 1): 
+            heap.append((-nums[l], l))
+        heapq.heapify(heap)
 
-print(Solution().maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3).pop)
+        l, r, index = 0, k - 1, 0 
+        init_max = float("inf") 
+        while r < len(nums): 
+            if l != 0: 
+                heapq.heappush(heap, (-nums[r], r))
+           
+            if index < l: 
+                heapq.heappop(heap)
+                init_max = heap[0][0]
+            new_max = heap[0][0]
+            if new_max < init_max:
+                init_max = new_max
+                index = heap[0][1]
+            
+            if index <= r and index >= l:
+                output.append(-init_max)
+            l += 1  
+            r += 1
+        
+        return output
+            
+        
+
+print(Solution().maxSlidingWindow([1 -1], 1))
 
