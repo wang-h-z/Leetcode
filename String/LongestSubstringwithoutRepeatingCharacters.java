ad2b0a4be077395bd3d2bcaa11f248e47588d451
WangHaozhen <whaozhen01@gmail.com>
2024-05-16 21:09:52 +0800

    Initial Commit

--- String/LongestSubstringwithoutRepeatingCharacters.java
+++ String/LongestSubstringwithoutRepeatingCharacters.java
@@ -1,5 +1,24 @@
 package String;
+import java.util.HashSet;
 
-public class LongestSubstringwithoutRepeatingCharacters {
-    
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        HashSet<Character> map = new HashSet<>();
+        int left = 0;
+        int right = 0;
+        
+        while (right < s.length()) {
+            while (map.contains(s.charAt(right))) {
+                map.remove(s.charAt(left));
+                left++;
+            }
+            map.add(s.charAt(right));
+            right++;
+            result = Math.max(result, right - left + 1);
+        }
+        
+        
+        return result;
+    }
 }
