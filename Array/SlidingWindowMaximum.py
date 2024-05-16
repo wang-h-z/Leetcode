a90e5f1b13a8c2939c5eaed51f30a314b8847610
WangHaozhen <whaozhen01@gmail.com>
2024-05-17 00:43:04 +0800

    Initial Commit

--- Array/SlidingWindowMaximum.py
+++ Array/SlidingWindowMaximum.py
@@ -2,5 +2,32 @@ from collections import deque
 from typing import List
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+        
+        # maintain maximum at the head of the queue
+        queue = deque() # queue will only store the index of elements in nums, queue maintains the window
+        output = []
+        
+        for i in range(len(nums)):
+            print(len(queue))
+            # check that the max element is not outside of the window while the queue is !empty
+            if queue and queue[0] <= i - k: 
+                queue.popleft()
+            
+            # pop right elements in the queue that are smaller than the current element (useless elements)
+            # this also maintains the largest element onto the left
+            while queue: 
+                if nums[queue[-1]] < nums[i]: 
+                    queue.pop()
+                else: 
+                    break
+            
+            # always append 
+            queue.append(i) 
+            
+            if i >= k - 1: 
+                output.append(nums[queue[0]])
+                
+        
+        return output
+
  
--- String/LongestReqpeatingCharacterReplacement.java
+++ String/LongestReqpeatingCharacterReplacement.java
@@ -2,6 +2,6 @@ package String;
 
 class Solution {
     public int characterReplacement(String s, int k) {
-        
+        return 0;
     }
 }
