8c28c9de2f1526e059f389de73850876eebc4d0a
WangHaozhen <whaozhen01@gmail.com>
2024-05-17 19:53:04 +0800

    Initial Commit

--- String/LongestReqpeatingCharacterReplacement.java
+++ String/LongestReqpeatingCharacterReplacement.java
@@ -1,7 +1,13 @@
 package String;
+import java.util.HashMap;
 
 class Solution {
     public int characterReplacement(String s, int k) {
-        return 0;
+        int res = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
+        return res;
     }
 }
