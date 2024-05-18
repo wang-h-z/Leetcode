85bbad46d5007424891ff2daf8b1847e20d79b75
WangHaozhen <whaozhen01@gmail.com>
2024-05-18 21:26:03 +0800

    fml

--- String/FIndAllAnagramsInAString.py
+++ String/FIndAllAnagramsInAString.py
@@ -1,24 +1,28 @@
 from typing import List
 
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
+    def findAnagrams(self, s: str, p: str): #-> List[int]:
         output = []
         left, right, counter = 0, 0, 0
-        table = set()
-        for i in p:
-            table.add(i)
-        while right < len(s):
+        table = {}
+        for index, char in enumerate(p):
+            table[char] = 0
+        while right < len(s): 
             if s[right] in table: 
-                table.remove(s[right])
-                if counter == len(p) - 1: 
-                    output.append(left)
-                    counter = 0
-                else : 
-                    counter += 1
-            else :
-                left += 1
-                counter == 0
-            right += 1
+                # check if frequeny is valid   
+                if counter == table.get(s[right]): 
+                    table[s[right]] = table[s[right]] + 1 
+                    if right - left + 1 == len(p): 
+                        counter += 1
+                        output.append(left) 
+                        left = right + 1
+                else: 
+                    left = right + 1
+            else: 
+                left = right + 1
+            
+            right += 1 
+        
         return output
     
 print(Solution().findAnagrams("cbaebabacd", "abc"))
