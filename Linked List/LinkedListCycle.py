c6908b83aca3c3780301e4efbfa7269abf93a0ec
WangHaozhen <whaozhen01@gmail.com>
2024-05-24 14:47:53 +0800

    a

--- Linked List/LinkedListCycle.py
+++ Linked List/LinkedListCycle.py
@@ -1,9 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+# Using Hash Table
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        map = dict()
+        while head: 
+            if head in map: 
+                return True
+            else: 
+                map[head] = 0
+            head = head.next
         return False
+
+# Using Floyd's Cycle Finding Algorithm / Tortoise and Hare algorithm
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
