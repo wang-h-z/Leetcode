da5c3aede47a9172d49496b0e59875216f517735
WangHaozhen <whaozhen01@gmail.com>
2024-05-24 21:01:56 +0800

    a

--- Recursion/GenerateParentheses.py
+++ Recursion/GenerateParentheses.py
@@ -1,4 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        output = []
+        output, sol = [], []
+        
+        def backtrack(open, close): 
+            
+            if len(sol) == 2*n: 
+                output.append(''.join(sol))
+                return 
+            
+            if open < n: # add a closing paranthesis ie -> (()
+                sol.append('(')
+                backtrack(open + 1, close)
+                sol.pop()
+            
+            if open > close: 
+                sol.append(')')
+                backtrack(open, close + 1)
+                sol.pop()
+                
+        backtrack(0,0)
         return output
